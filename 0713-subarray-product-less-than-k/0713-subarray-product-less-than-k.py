ac627f09a8b303e6bdedd719f94163673f50b2e9
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-06-29 18:39:37 -0400

    Time: 495 ms (64.31%), Space: 19.1 MB (58.44%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,23 +1,14 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # Handle edge cases where k is 0 or 1 (no subarrays possible)
         if k <= 1:
             return 0
-
-        total_count = 0
-        product = 1
-
-        # Use two pointers to maintain a sliding window
+        curr = 1
         left = 0
-        for right, num in enumerate(nums):
-            product *= num  # Expand the window by including the element at the right pointer
-
-            # Shrink the window from the left while the product is greater than or equal to k
-            while product >= k:
-                product //= nums[left]  # Remove the element at the left pointer from the product
+        ans = 0
+        for right in range(len(nums)):
+            curr *= nums[right]
+            while curr >= k and left <= right:
+                curr //= nums[left]
                 left += 1
-
-            # Update the total count by adding the number of valid subarrays with the current window size
-            total_count += right - left + 1  # right - left + 1 represents the current window size
-
-        return total_count
+            ans += right - left + 1
+        return ans
