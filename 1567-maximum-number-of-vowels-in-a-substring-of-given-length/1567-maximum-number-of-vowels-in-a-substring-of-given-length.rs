ee8a300a800597b068ceee34129c24aba1f63e51
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-07-04 16:26:40 -0400

    Time: 14 ms (9.02%), Space: 2.7 MB (32.33%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.rs
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.rs
@@ -1,30 +1,25 @@
 use std::collections::HashSet;
-
 impl Solution {
     pub fn max_vowels(s: String, k: i32) -> i32 {
-        let k = k as usize; // Convert k to usize since we'll use it as an index
-        let vowels: HashSet<char> = vec!['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].into_iter().collect();
-        let chars: Vec<char> = s.chars().collect(); // Collect string characters into a vector
-
-        let mut current_vowels = 0;
-        for i in 0..k {
-            if vowels.contains(&chars[i]) {
-                current_vowels += 1;
+        let k : usize = k as usize;
+        let mut currVowels = 0;
+        let vowels: HashSet<char> = vec!['a', 'e', 'i', 'o', 'u'].into_iter().collect();
+        let chars: Vec<char> = s.chars().collect();
+        for i in 0..k{
+            if vowels.contains(&chars[i]){
+                currVowels += 1;
             }
         }
-        
-        let mut max_vowels = current_vowels;
-        
-        for i in k..chars.len() {
-            if vowels.contains(&chars[i]) {
-                current_vowels += 1;
+        let mut maxVowels = currVowels;
+        for i in k..chars.len(){
+            if vowels.contains(&chars[i]){
+                currVowels += 1;
             }
-            if vowels.contains(&chars[i - k]) {
-                current_vowels -= 1;
+            if vowels.contains(&chars[i - k]){
+                currVowels -= 1;
             }
-            max_vowels = max_vowels.max(current_vowels);
+            maxVowels = maxVowels.max(currVowels);
         }
-        
-        max_vowels
+        maxVowels
     }
 }
