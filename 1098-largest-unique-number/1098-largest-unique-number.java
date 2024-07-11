665e4f6418159b22e8355ea5cde7e965595eedc9
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-07-11 11:16:42 -0400

    Time: 1 ms (99.28%), Space: 42.8 MB (50%) - LeetHub

--- 1098-largest-unique-number/1098-largest-unique-number.java
+++ 1098-largest-unique-number/1098-largest-unique-number.java
@@ -1,15 +1,24 @@
 class Solution {
     public int largestUniqueNumber(int[] nums) {
-        HashMap<Integer, Integer> mp = new HashMap();
-        for(int i : nums){
-            mp.put(i, mp.getOrDefault(i, 0)+1);
+        // HashMap<Integer, Integer> mp = new HashMap();
+        // for(int i : nums){
+        //     mp.put(i, mp.getOrDefault(i, 0)+1);
+        // }
+        // int maximum = Integer.MIN_VALUE;
+        // for(Map.Entry<Integer, Integer> entry: mp.entrySet()){
+        //     if (entry.getValue() == 1){
+        //         maximum = Math.max(maximum, entry.getKey());
+        //     }
+        // }
+        // return Math.max(maximum, -1);
+         int[] counts = new int[1001];
+        for (int num : nums) {
+            counts[num]++;
         }
-        int maximum = Integer.MIN_VALUE;
-        for(Map.Entry<Integer, Integer> entry: mp.entrySet()){
-            if (entry.getValue() == 1){
-                maximum = Math.max(maximum, entry.getKey());
-            }
+        for (int i = 1000; i >= 0; i--) {
+            if (counts[i] == 1)
+                return i;
         }
-        return Math.max(maximum, -1);
+        return -1;
     }
 }
