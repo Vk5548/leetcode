6884fb661942065c45e2b820f2a283b2b3d9d8d8
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-07-11 17:59:12 -0400

    Time: 40 ms (40.38%), Space: 16.7 MB (27.45%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -14,5 +14,14 @@ class Solution:
             mini = min(mini, val)
         return mini
 
+    # def maxNumberOfBalloons(self, text: str) -> int:
+    #     b = text.count("b")
+    #     a = text.count("a")
+    #     l = text.count("l")
+    #     o = text.count("o")
+    #     n = text.count("n")
+
+    #     return min(b, a, l // 2, o // 2, n)
+
 
         
