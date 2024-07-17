4cc3b3f85c360e65253666dc83c431aa592d2430
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-07-17 19:38:30 -0400

    Time: 15 ms (9.62%), Space: 6.1 MB (18.44%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.rs
+++ 0049-group-anagrams/0049-group-anagrams.rs
@@ -1,13 +1,29 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        let mut anagram : HashMap<String, Vec<String>> = HashMap::new();
-        for stro in strs{
-            let mut chrs = stro.chars().collect::<Vec<char>>();
-            chrs.sort_unstable();
-            let key = chrs.into_iter().collect::<String>();
-            anagram.entry(key).or_insert(Vec::new()).push(stro);
+        // let mut anagram : HashMap<String, Vec<String>> = HashMap::new();
+        // for stro in strs{
+        //     let mut chrs = stro.chars().collect::<Vec<char>>();
+        //     chrs.sort_unstable();
+        //     let key = chrs.into_iter().collect::<String>();
+        //     anagram.entry(key).or_insert(Vec::new()).push(stro);
+        // }
+        // anagram.into_values().collect()
+
+        //NEW Solution
+        let mut cache : HashMap<[i32; 26], Vec<String>> = HashMap::new();
+        for s in strs{
+            let mut charArray = s.chars().collect::<Vec<char>>();
+            let mut letters: [i32; 26] = [0; 26];
+            for c in charArray{
+                print!("c iz {} ", c);
+                letters[(c as u8 - 'a' as u8) as usize] += 1;
+            }
+            match cache.get_mut(&letters) {
+                None => { cache.insert(letters, vec![s]);}
+                Some(v) => v.push(s)
+            }
         }
-        anagram.into_values().collect()
+        cache.into_values().collect()
     }
 }
