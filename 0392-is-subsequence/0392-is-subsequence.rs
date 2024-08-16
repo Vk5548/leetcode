b85ae2a92fa25d6a3a296b54bcac66a76c8e67f8
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-08-16 09:39:59 -0400

    Time: 2 ms (14.47%), Space: 2.2 MB (46.38%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn is_subsequence(s: String, t: String) -> bool {
+        let (mut i, mut j) = (0, 0);
+        let vec_s = s.chars().collect::<Vec<char>>();
+        let vec_t = t.chars().collect::<Vec<char>>();
+        println!("{:?}", vec_t);
+        while i < s.len() && j < t.len(){
+            if vec_s[i] == vec_t[j]{
+                println!("{} {}", vec_s[i], vec_t[j]);
+                i += 1;
+            }
+            j += 1;
+        }
+        
+        if i >= s.len(){
+            true
+        }else{
+            false
+        }
+    }
+}
