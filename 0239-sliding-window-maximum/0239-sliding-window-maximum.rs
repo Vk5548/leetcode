c6fb2a87219eaa6a6909397fcdbae6c7756d9e74
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-10-09 13:58:28 -0400

    Time: 48 ms (54.05%), Space: 3.5 MB (80.18%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.rs
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.rs
@@ -2,47 +2,28 @@ use std::collections::VecDeque;
 
 impl Solution {
     pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
-        let k = k as usize; // Convert k to usize
-        if nums.is_empty() || k == 0 {
-            return vec![];
-        }
+        let mut left: usize = 0;
         let mut queue: VecDeque<usize> = VecDeque::new();
-        let mut result = Vec::new();
-
-        // Process the first k elements
-        for i in 0..k {
-            while let Some(&last) = queue.back() {
-                if nums[last] <= nums[i] {
+        let mut res : Vec<i32> = Vec::new();
+        for right in 0..nums.len(){
+            while let Some(&q) = queue.back(){
+                if nums[q] < nums[right]{
                     queue.pop_back();
-                } else {
-                    break;
-                }
-            }
-            queue.push_back(i);
-        }
-        result.push(nums[*queue.front().unwrap()]);
-
-        // Process the rest of the elements
-        for right in k..nums.len() {
-            // Remove indices that are out of the current window
-            if let Some(&front) = queue.front() {
-                if front <= right - k {
-                    queue.pop_front();
-                }
-            }
-
-            // Remove indices whose corresponding values are less than or equal to nums[right]
-            while let Some(&last) = queue.back() {
-                if nums[last] <= nums[right] {
-                    queue.pop_back();
-                } else {
+                }else{
                     break;
                 }
             }
             queue.push_back(right);
-
-            result.push(nums[*queue.front().unwrap()]);
+            if right - left + 1 == k as usize{
+                if let Some(&q) = queue.front(){
+                    res.push(nums[q]);
+                    if q == left{
+                        queue.pop_front();
+                    }
+                    left += 1;
+                }
+            }
         }
-        result
+        res
     }
 }
