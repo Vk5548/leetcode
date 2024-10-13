5fa6b80bd1c94603e4ac197c0aaed6a7b2c4ceb1
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-10-13 17:23:21 -0400

    Time: 1 ms (78.81%), Space: 2.6 MB (98.14%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
@@ -20,14 +20,38 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        match root{
-            Some(root) => {
-                let root = root.borrow();
-                let left = Solution::max_depth(root.left.clone()); // No idea about the parent
-                let right = Solution::max_depth(root.right.clone());
-                return 1 + left.max(right);
-            },
-            None => 0,
+        // match root{
+        //     Some(root) => {
+        //         let root = root.borrow();
+        //         let left = Solution::max_depth(root.left.clone()); // No idea about the parent
+        //         let right = Solution::max_depth(root.right.clone());
+        //         return 1 + left.max(right); // logic for the current node; therefore, it post order traversal
+        //     },
+        //     None => 0,
+        // }
+        if root.is_none(){
+            return 0;
         }
+
+        let mut stack: Vec<(Option<Rc<RefCell<TreeNode>>>, i32)> = Vec:: new();
+
+        let mut max_depth = 0;
+
+        stack.push((root, 1));
+        while let Some((node, depth)) = stack.pop(){
+            if let Some(node_ref) = node{
+                let node = node_ref.borrow();
+
+                max_depth = max_depth.max(depth);
+
+                if node.left.is_some(){
+                    stack.push((node.left.clone(), depth + 1));
+                }
+                if node.right.is_some(){
+                    stack.push((node.right.clone(), depth + 1));
+                }
+            }
+        }
+        max_depth
     }
 }
