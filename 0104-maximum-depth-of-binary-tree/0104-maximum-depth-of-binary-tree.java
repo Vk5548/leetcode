eceb1f7cfad74c2b39ed147d13a3d712f62219a4
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-10-13 16:30:45 -0400

    Time: 2 ms (11.53%), Space: 42.9 MB (11%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -13,15 +13,41 @@
  *     }
  * }
  */
+
+import java.util.Stack;
+class Pair{
+    TreeNode node;
+    int depth;
+    Pair(TreeNode node, int depth){
+        this.node = node;
+        this.depth = depth;
+    }
+}
 class Solution {
     public int maxDepth(TreeNode root) {
+        //Iterative approach;
         if (root == null){
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
 
-        return 1 + Math.max(left, right);
+        Stack<Pair> stk = new Stack<>();
+        stk.push(new Pair(root, 1));
+        int ans = 0;
 
+        while(!stk.isEmpty()){
+            Pair pair = stk.pop();
+            TreeNode node = pair.node;
+            int depth = pair.depth;
+
+            ans = Math.max(ans, depth);
+            if(node.left != null){
+                stk.push(new Pair(node.left, depth + 1));
+            }
+
+            if(node.right != null){
+                stk.push(new Pair(node.right, depth + 1));
+            }
+        }
+        return ans;
     }
 }
