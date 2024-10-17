39ddb21a383091f51e803e6714b7c70179c081b4
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-10-17 14:32:57 -0400

    Time: 0 ms (100%), Space: 41.7 MB (6.77%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,18 +13,55 @@
  *     }
  * }
  */
+
+class Pair{
+    TreeNode n;
+    char pos;
+    Pair(TreeNode n, char pos){
+        this.n = n;
+        this.pos = pos;
+    }
+}
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        //base cases
+        //base case
         if( p == null && q == null){
             return true;
         }
-        if(p == null || q == null || p.val != q.val){
+        if(p == null || q== null){
             return false;
         }
+        Stack<Pair> s1 = new Stack<>();
+        Stack<Pair> s2 = new Stack<>();
+        s1.push(new Pair(p, '_'));
+        s2.push(new Pair(q, '_'));
 
-        boolean left = isSameTree(p.left, q.left);
-        boolean right = isSameTree(p.right, q.right);
-        return left && right;
+        while(!s1.isEmpty() && !s2.isEmpty()){
+            Pair p1 = s1.pop();
+            Pair q1 = s2.pop();
+            TreeNode pTree = p1.n;
+            TreeNode qTree = q1.n;
+            char posP = p1.pos;
+            char posQ= q1.pos;
+            
+
+            if(posP != posQ || pTree.val != qTree.val){
+                return false;
+            }
+            
+            if(pTree.right != null && qTree.right != null){
+                s1.push(new Pair(pTree.right, 'r'));
+                s2.push(new Pair(qTree.right, 'r'));
+            }else if (pTree.right != null || qTree.right != null) {
+                return false;
+            }
+            if(pTree.left != null && qTree.left != null){
+                s1.push(new Pair(pTree.left, 'l'));
+                s2.push(new Pair(qTree.left, 'l'));
+            }else if (pTree.left != null || qTree.left != null) {
+                return false;
+            }
+        }
+        return true;
     }
 }
