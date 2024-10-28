48745125ee7431fd39e6a046d1a95552e5e6440d
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-10-28 12:55:17 -0400

    Time: 1 ms (100%), Space: 45.1 MB (38.7%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,39 +1,51 @@
 class Solution {
-    boolean[] visited ;
-    Map<Integer, List<Integer>> graph = new HashMap<>();
+    int parents[];
+    int rank[];
+    int numComponents;
     public int countComponents(int n, int[][] edges) {
-        //making an adjacency lits:
-        
-        for(int[] edge: edges){
-            int a = edge[0];
-            int b = edge[1];
-            graph.putIfAbsent(a, new ArrayList<>());
-            graph.putIfAbsent(b, new ArrayList<>());
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-
-        visited = new boolean[n];
-        int numOfComponents = 0;
+        // solving it using union-find:
+        parents = new int[n];
+        rank = new int[n];
+        numComponents = n;
         for(int i = 0; i < n; i++){
-            //starting dfs from every unvisited node;
-            if(!visited[i]){
-                visited[i] = true;
-                numOfComponents++;
-                dfs(i);
-            }
+            parents[i] = i; //each node will be its own parent initially
+            rank[i] = 1; //each component is of size = 1 initially
         }
-        return numOfComponents;
+        for(int[] edge: edges){
+            int p = edge[0];//parent
+            int c = edge[1]; //child
+            //so child's parent will change
+            
+            union(p, c);
+
+        }
+        return numComponents;
     }
-    private void dfs(int node){
-        if(graph.get(node) == null){
+    private void union(int parent, int child){
+        int rootX = findParent(parent);
+        int rootY = findParent(child);
+
+        //if they have the same parent: do nothing
+        if(rootX == rootY){
             return;
         }
-        for(int neighbor : graph.get(node)){
-            if(!visited[neighbor]){
-                visited[neighbor] = true;
-                dfs(neighbor);
-            }
+
+        //if that's not the case:
+        //merging will happen:
+        numComponents--;
+         // Union by rank
+        if (rank[rootX] > rank[rootY]) {
+            parents[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parents[rootX] = rootY;
+        } else { // rank[rootX] == rank[rootY]
+            parents[rootY] = rootX;
+            rank[rootX]++;
         }
+
+    }
+    private int findParent(int child){
+        if(parents[child] == child) return child;
+        return findParent(parents[child]);
     }
 }
