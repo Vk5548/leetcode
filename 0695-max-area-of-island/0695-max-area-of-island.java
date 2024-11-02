69587dec477a79874b6edc4abcc8f19e217d0207
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-02 14:22:21 -0400

    Time: 2 ms (67.91%), Space: 44.5 MB (48.97%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,41 +1,35 @@
 class Solution {
     int m, n;
-    int[][] seen;
-    int[][] directions = new int[][]{{0,-1}, {0, 1}, {-1, 0}, {1, 0}};
-    
+    int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}; //explain this to interviewer
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = Integer.MIN_VALUE;
         m = grid.length;
         n = grid[0].length;
-        seen = new int[m][n];
-        int CurrArea = 0;
-        for(int i = 0; i < m; i++){
+        int maxArea = Integer.MIN_VALUE, currArea = 0;
+
+        for(int i = 0; i < m ; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1 && seen[i][j] == 0){ //if Land && not seen
-                    seen[i][j] = 1;
-                    CurrArea = dfs(i, j, grid);
-                    System.out.println(CurrArea);
-                    maxArea = Math.max(maxArea, CurrArea);
-                    CurrArea = 0;
+                if(grid[i][j] == 1){
+                    currArea = dfs(i, j, grid);
+                    maxArea = Math.max(maxArea, currArea);
                 }
             }
         }
-        return maxArea != Integer.MIN_VALUE ? maxArea : 0;
+        return maxArea != Integer.MIN_VALUE ? maxArea : 0 ;
     }
-    private boolean isValid(int x, int y){
-        return x >= 0 && x < m && y >= 0 && y < n;
+    private boolean isValid(int x, int y, int[][] grid){
+        return x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1;
     }
-    private int dfs(int i, int j, int[][] grid){
-        int area = 1;
-        //getting neighbors:
-        for(int[] dir : directions){ //dir = [x,y]
-            int newI = i + dir[0], newJ = j + dir[1];
-            if(isValid(newI, newJ) && grid[newI][newJ] == 1 && seen[newI][newJ] == 0){
-                seen[newI][newJ] = 1;
-                area  += dfs(newI, newJ, grid);
-
+    private int dfs(int row, int col, int[][] grid){
+        //mark it visited
+        grid[row][col] = 0;
+        int result = 1;
+        //visiting the neighbors
+        for(int[] dir: directions){
+            int newRow = row + dir[0], newCol = col + dir[1];
+            if(isValid(newRow, newCol, grid)){
+                result += dfs(newRow, newCol, grid);
             }
         }
-        return area;
+        return result;
     }
 }
