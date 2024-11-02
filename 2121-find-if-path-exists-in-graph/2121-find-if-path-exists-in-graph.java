9066240f258e62b33cf1b80a61ad53f3c3c526c6
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-02 13:52:39 -0400

    Time: 318 ms (10.38%), Space: 204.8 MB (10.17%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,37 +1,33 @@
 class Solution {
+    Map<Integer, List<Integer>> graph = new HashMap<>();
+    int[] seen;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        int[] visited = new int[n];
-        //prerocessing the graph
-        // I have edges: an array
-        // I am thinking to make an adjacency list??
-        //So, I think I will use a HAshMap?? : O(1): look-up operation
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int[] edge : edges){
+       for(int[] edge: edges){ //traversing through the edges
             int u = edge[0];
             int v = edge[1];
             graph.putIfAbsent(u, new ArrayList<>());
             graph.putIfAbsent(v, new ArrayList<>());
             graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-        visited[source] = 1;
-        dfs(source, visited, graph);
-        return visited[destination] == 1;
-
+            graph.get(v).add(u); // since its bi-directional
+       }// graph ready
+        seen = new int[n];
+        //visit the source:
+        seen[source] = 1;
+        dfs(source);
+        return seen[destination] == 1;
     }
-    private void dfs(int source, int[] visited, Map<Integer, List<Integer>> graph){
-        // getting the neighbors of the source node and running dfs on it
+    //recursive dfs
+    private void dfs(int source){
+        // to get the neighbors of any node, we will need the list value of the graph
         if(graph.get(source) == null){
             return;
         }
-        for(Integer neighbor: graph.get(source)){
-            if(neighbor != null){
-                if(visited[neighbor] == 0){
-                    visited[neighbor] = 1;
-                    dfs(neighbor, visited, graph);
+        for(int neighbor : graph.get(source)){
+            if(seen[neighbor] == 0){
+                seen[neighbor] = 1;
+                dfs(neighbor);
             }
         }
-            
-        }
+
     }
 }
