7859206748086282f5ececf26d9a8607be4092c1
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-03 12:52:17 -0500

    Time: 33 ms (41.24%), Space: 50.9 MB (71.22%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((n1, n2) -> {
+           // Calculate distances and compare
+            int d1 = n1[0] * n1[0] + n1[1] * n1[1];
+            int d2 = n2[0] * n2[0] + n2[1] * n2[1];
+            return d2 - d1; 
+        });
+
+        for(int[] point: points){
+            heap.add(point);
+            if(heap.size() > k){
+                heap.poll();
+            }
+        }
+
+        int[][] res = new int[k][2];
+        int i = 0;
+        while(heap.size() > 0){
+            res[i++] = heap.poll();
+        }
+        return res;
+    }
+}
