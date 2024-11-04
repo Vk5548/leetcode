cfb54d9c71ba881866edbd75db02809ad7dca57e
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-03 23:31:40 -0500

    Time: 0 ms (100%), Space: 41.9 MB (66.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,22 +1,30 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length, col = matrix[0].length;
-        for(int i = 0; i < rows ; i++){
-            //find the row
-            if(target >= matrix[i][0] && target <= matrix[i][col- 1]){
-                int left = 0, right = col, mid = -1;
-                while(left <= right){
-                    mid = (left + right) / 2;
-                    if(target == matrix[i][mid]){
-                        return true;
-                    }else if( target > matrix[i][mid]){
-                        left = mid + 1;
-                    }else{
-                        right = mid - 1;
-                    }
-                }
-                return false;
+        // we need to check for rows; which row contains the value 
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int targetRow = 0;
+        for(targetRow = 0; targetRow< rows; targetRow++){
+            if(target >= matrix[targetRow][0] && target <= matrix[targetRow][cols -1]){
+                break;
             }
+        }
+        if(targetRow >= 0 && targetRow < rows){
+            //start the bianry search just on this row
+            int left = 0, right = cols - 1;
+
+            while(left <= right){
+               int mid = left + (right - left)/2;
+
+                if(target == matrix[targetRow][mid]){
+                    return true;
+                }else if(target > matrix[targetRow][mid]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+           
         }
         return false;
     }
