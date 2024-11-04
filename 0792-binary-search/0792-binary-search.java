2ddd96d6abb4c958addb0a2615cfdd52f230d117
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-03 22:09:36 -0500

    Time: 0 ms (100%), Space: 45.7 MB (34.12%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2; // to prevent overflow
+
+            if(target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
