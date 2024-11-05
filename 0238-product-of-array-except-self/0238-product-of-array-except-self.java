15aa03f9d73b838bdaef2f6856fc2ec4e96a8bbd
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-05 12:15:39 -0500

    Time: 2 ms (88.83%), Space: 55.5 MB (46.28%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // case no 0; then it works normally
+        //case 1: one 0; we need to store the product other numbers to enter this
+        //case 2: more than 1 zer0;
+
+        int ctr0 = 0; int idx = -1;
+        int productWithoutZero = 1, product = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                ctr0++;
+                idx = i;
+                if(ctr0 > 1){ //at any point
+                    return new int[nums.length];
+                }
+                productWithoutZero = product;
+            }else{
+                product *= nums[i];
+                productWithoutZero *= nums[i];
+            }
+        }
+        int[] res = new int[nums.length];
+        if(ctr0 == 1){  
+            res[idx] = productWithoutZero;
+            
+        }else{
+            for(int i = 0; i < nums.length; i++){
+                res[i] = product/ nums[i];
+            }
+        }
+        return res;
+    }
+}
