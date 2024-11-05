e2d03a28128c835e7e30b71352c0fdc31fe33cbd
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-04 22:35:31 -0500

    Time: 1 ms (84.4%), Space: 42.8 MB (84.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,41 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        backtrackGenerate(n, "", 0, 0, result);
-        return result;
-    }
+    // public static boolean check(String s) {
+    //     int x = 0;
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(')
+    //             x++;
+    //         else
+    //             x--;
+    //         if (x < 0)
+    //             return false;
+    //     }
+    //     return x == 0;
+    // }
 
-    public void backtrackGenerate(int n, String currentCombination, int open, int close, List<String> result){
-        if(currentCombination.length() == n * 2){
-            result.add(currentCombination);
+    public void solve(int n, int open, int close, List<String> ans, StringBuilder builder) {
+        if (open + close == 2*n) {
+            String s = builder.toString();
+            ans.add(s);
             return;
         }
+        if(open < n){
+            builder.append('(');
+            solve(n, open + 1, close, ans, builder);
+            if(builder.length() > 0)
+                builder.deleteCharAt(builder.length() - 1);
+        }
+        if(close < open) {
+            builder.append(')');
+            solve(n, open, close+1, ans, builder);
+            if(builder.length() > 0)
+                builder.deleteCharAt(builder.length() - 1);
+        }
+    }
 
-        if(open < n)
-            backtrackGenerate(n, currentCombination + "(" , open + 1, close, result);
-        if(close < open)
-            backtrackGenerate(n, currentCombination + ")", open, close + 1, result);
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<String>();
+        StringBuilder builder = new StringBuilder();
+        solve(n, 0, 0, ans, builder);
+        return ans;
     }
 }
