c5fb6552c049c474819d1b23c67b07882b048fc0
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-05 12:32:56 -0500

    Time: 2 ms (88.83%), Space: 55.5 MB (46.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,34 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // case no 0; then it works normally
-        //case 1: one 0; we need to store the product other numbers to enter this
-        //case 2: more than 1 zer0;
+        int n = nums.length;
+        int[] result = new int[n];
 
-        int ctr0 = 0; int idx = -1;
-        int productWithoutZero = 1, product = 1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                ctr0++;
-                idx = i;
-                if(ctr0 > 1){ //at any point
-                    return new int[nums.length];
-                }
-                productWithoutZero = product;
-            }else{
-                product *= nums[i];
-                productWithoutZero *= nums[i];
-            }
+        // Step 1: Calculate prefix products and store in result
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
         }
-        int[] res = new int[nums.length];
-        if(ctr0 == 1){  
-            res[idx] = productWithoutZero;
-            nums = new int[nums.length];
-            nums[idx] = productWithoutZero;
-        }else{
-            for(int i = 0; i < nums.length; i++){
-                nums[i] = product/ nums[i];
-            }
+
+        // Step 2: Calculate postfix products and multiply with prefix products in result
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= postfix;
+            postfix *= nums[i];
         }
-        return nums;
+
+        return result;
     }
 }
