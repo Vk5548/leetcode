f3b57ac7a63c826212d41b9386e506ae046eee3a
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2024-11-05 18:12:13 -0500

    Time: 9 ms (48.89%), Space: 42.4 MB (83.94%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        int[] copyCount = new int[26];
+        for(int i =0; i < s1.length(); i++){
+            int k = s1.charAt(i) - 'a';
+            count[k]++;
+            // the 2nd string, only getting the current window
+            k = s2.charAt(i) - 'a';
+            copyCount[k]++;
+        }
+        // track of matches
+        int matches = 0;
+        for(int i =0;i < 26; i++ ){
+            if(count[i]== copyCount[i])
+                matches++;
+        }
+        // for any sliding window problem : we need a left and a write pointer
+        int left = 0;
+        for(int right =s1.length(); right < s2.length(); right++)
+        {
+            if(matches == 26) // if all letters are present in both substring
+                return true;
+            int k = s2.charAt(right) - 'a';
+            copyCount[k]++;
+            if(count[k] == copyCount[k])
+                matches++;
+            if(count[k] + 1 == copyCount[k])
+                matches--;
+
+            k = s2.charAt(left) - 'a';
+            copyCount[k]--;
+            if(count[k] == copyCount[k])
+                matches++;
+             if(count[k] - 1 == copyCount[k])
+                matches--;
+            left++;
+        }     
+        return matches == 26 ;
+    }
+}
