f580c6ad98249da3d357548a061ce5e46a73eaa8
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-01-21 18:58:46 -0500

    Time: 0 ms (100%), Space: 2.6 MB (32%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.rs
+++ 0917-boats-to-save-people/0917-boats-to-save-people.rs
@@ -3,28 +3,23 @@ impl Solution {
         if people.is_empty(){
             return 0;
         }
-        people.sort_unstable(); // Sort people by weight
-        let mut left = 0; // Start pointer (lightest person)
-        let mut right = people.len() - 1; // End pointer (heaviest person)
-        let mut num_boats = 0; // Boat counter
+        people.sort_unstable();
+        let mut num_boats = 0;
+        let mut right = people.len() - 1;
+        let mut left = 0;
 
-        
-        while left <= right {
-            println!("left: {}, right: {}", left, right); 
-            if people[left] + people[right] <= limit {
-                // If the lightest and heaviest person can fit in one boat, pair them
+        while left < right {
+            num_boats += 1;
+            if people[left] + people[right] <= limit{ //paired
                 left += 1;
             }
-            // Either way, the heaviest person goes on the boat
-            
-            
-            num_boats += 1; // Increment the boat counter
-            if right == 0{
-                break;
-            }
+
             right -= 1;
         }
         
-        num_boats // Return the total number of boats
+        if left == right{
+            num_boats += 1;
+        }
+        num_boats
     }
 }
