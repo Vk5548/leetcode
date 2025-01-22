d999555f199374b73bbe844103013d298805b95f
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-01-22 17:13:36 -0500

    Time: 6 ms (80%), Space: 2.6 MB (56%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.rs
+++ 0917-boats-to-save-people/0917-boats-to-save-people.rs
@@ -4,22 +4,20 @@ impl Solution {
             return 0;
         }
         people.sort_unstable();
-        let mut num_boats = 0;
-        let mut right = people.len() - 1;
+        let mut boats = 0;
         let mut left = 0;
-
+        let mut right = people.len() - 1;
         while left < right {
-            num_boats += 1; //regardless, because heavier are gonna go anyways
-            if people[left] + people[right] <= limit{ //paired
+            if left != right && people[left] + people[right] <= limit { // can be paired
                 left += 1;
             }
 
             right -= 1;
+            boats += 1;
         }
-        
         if left == right{
-            num_boats += 1;
+            boats += 1;
         }
-        num_boats
+        boats
     }
 }
