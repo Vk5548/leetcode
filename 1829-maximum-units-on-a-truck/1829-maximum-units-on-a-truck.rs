3996a4bc652932f0119a8f4bea845a3f582546ee
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-01-24 10:28:39 -0500

    Time: 55 ms (8.33%), Space: 2.3 MB (58.33%) - LeetHub

---
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn maximum_units(mut box_types: Vec<Vec<i32>>, truck_size: i32) -> i32 {
+        box_types.sort_by(|a,b| b[1].cmp(&a[1]));//sorted by the frequency
+
+        //now iterate through the box_types
+        let mut num_boxes = 0;
+        let mut num_units = 0;
+        print!("{:?}", box_types);
+        for i in 0..box_types.len(){
+           
+            if num_boxes + box_types[i][0] <= truck_size { //all boxes can be added
+                num_boxes += box_types[i][0];
+                num_units += box_types[i][0] * box_types[i][1];
+            }else if num_boxes < truck_size{ //partial addition of boxes
+                //how many boxes to add
+                let balance = truck_size - num_boxes;
+                num_boxes += balance;
+                num_units += balance * box_types[i][1];
+                break;
+            }else{//no more addition
+                break;
+            }
+             println!("num_boxes after everi iter {}", num_boxes);
+            println!("num_units after everi iter {}", num_units);
+        }
+         println!("num_boxes after everi iter {}", num_boxes);
+            println!("num_units after everi iter {}", num_units);
+        num_units
+    }
+}
