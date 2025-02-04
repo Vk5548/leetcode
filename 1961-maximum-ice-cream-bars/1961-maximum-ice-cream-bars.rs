51e03933e88d240ef5202bdbaa90ceb72625a449
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-04 10:05:45 -0500

    Time: 14 ms (14.29%), Space: 3.9 MB (28.57%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.rs
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.rs
@@ -1,31 +1,26 @@
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
 impl Solution {
     pub fn max_ice_cream(costs: Vec<i32>, coins: i32) -> i32 {
-        let max_num = *costs.iter().max().unwrap_or(&0);
+        let mut min_heap : BinaryHeap<Reverse<i32>> = BinaryHeap::new();
 
-        //declare the array for this size
-        let mut freq = vec![0; (max_num+ 1) as usize];
+        //iterate through the costs
+        for &cost in costs.iter(){
+            min_heap.push(Reverse(cost));
+        }//set up
 
-        for &c in &costs{
-            freq[c as usize] += 1;
-        } // got the freq array // meanin the index is the cost
-
-        let mut remanining_coins = coins;
-        let mut bars_bought = 0;
-
-        for cost in 1..=max_num as usize{
-            //how many bars are available to buy
-            let num_bars = freq[cost];
-
-            //how many bars can be bought
-            let bars_can_be_bought = (remanining_coins/cost as i32).min(num_bars);
-
-            bars_bought += bars_can_be_bought;
-            remanining_coins -= bars_can_be_bought * cost as i32;
-
-            if remanining_coins<= 0{
+        let mut bars = 0;
+        let mut coins = coins;
+        while let Some(Reverse(curr)) = min_heap.pop(){
+            coins -= curr;
+            if coins < 0{
                 break;
             }
+
+            bars+= 1;
+            
         }
-        bars_bought
+        bars
     }
 }
