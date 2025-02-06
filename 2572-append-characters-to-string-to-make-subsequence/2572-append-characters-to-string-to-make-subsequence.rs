fe26096cb5d009753959a75a93e013dd87c939b2
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-06 18:38:57 -0500

    Time: 0 ms (100%), Space: 2.5 MB (62.5%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.rs
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.rs
@@ -1,21 +1,12 @@
 impl Solution {
     pub fn append_characters(s: String, t: String) -> i32 {
-        let mut s = s.into_bytes();
-        let mut t = t.into_bytes();
-       
-        let mut s_len = s.len();
-        let mut t_len = t.len();
-
-        let mut s_idx = 0;
-        let mut t_idx = 0;
-        while s_idx < s_len && t_idx < t_len{
-            if t[t_idx] == s[s_idx]{
-                t_idx += 1;
-                s_idx += 1;   
-            }else{
-                s_idx += 1;
-            }
+        let mut s = &*s;
+        for (i, c) in t.chars().enumerate(){
+            let Some((_, rest)) = s.split_once(c) else {
+                return (t.len() - i) as i32;
+            };
+            s = rest;
         }
-        t[t_idx..].len() as i32
+        0
     }
 }
