5b013badbfd2aa887af7b1aa1e5e774b220161fc
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-05 19:47:04 -0500

    Time: 3 ms (33.33%), Space: 2.5 MB (100%) - LeetHub

--- 1782-smallest-string-with-a-given-numeric-value/1782-smallest-string-with-a-given-numeric-value.rs
+++ 1782-smallest-string-with-a-given-numeric-value/1782-smallest-string-with-a-given-numeric-value.rs
@@ -1,26 +1,16 @@
 impl Solution {
-    pub fn get_smallest_string(n: i32, k: i32) -> String {
-        //result_string
-        let mut result = vec!['a' ; n as usize]; // of size n
+    pub fn get_smallest_string(mut n: i32, mut k: i32) -> String {
+        let mut res = Vec::with_capacity(n as usize);
 
-        //calculate the extra
-        let mut extra = k - n;
-
-        let mut i = n as usize -1;
-
-        while extra > 0 {
-            let add = extra.min(25); //sice at-most 25 can be added to make it to z
-
-            result[i] = (result[i] as u8 + add as u8) as char;
-
-            extra -= add;
-
-            if extra <= 0{
-                break;
-            }
-            i -= 1;
-        } 
-        result.into_iter().collect()
-
+        while n < k { //counting for the extra variables
+            let curr = (k - n).min(25);
+            //add the curr character to the end
+            res.push(b'a'  + curr as u8) ;
+            k -= curr + 1;
+            n -= 1;
+        }
+        res.extend(std::iter::repeat(b'a').take(n as usize));
+        res.reverse();
+        String::from_utf8(res).unwrap()
     }
 }
