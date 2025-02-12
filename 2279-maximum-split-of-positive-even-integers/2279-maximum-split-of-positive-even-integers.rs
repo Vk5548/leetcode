04a2200bee2d0960bfdb62174c68257a176b9a65
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-12 14:35:58 -0500

    Time: 0 ms (100%), Space: 3.6 MB (0%) - LeetHub

---
+++ 2279-maximum-split-of-positive-even-integers/2279-maximum-split-of-positive-even-integers.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn maximum_even_split(final_sum: i64) -> Vec<i64> {
+        if final_sum % 2 == 1{
+            return vec![];
+        }
+
+        let mut res = Vec::new();
+
+        let mut curr_num = 2;
+        let mut remainder = final_sum;
+
+        while remainder >= curr_num{
+            res.push(curr_num);
+            remainder -= curr_num;
+            curr_num += 2;
+        }
+
+        // if there is aremainder remaining
+        if let Some(last_num) = res.last_mut(){
+            *last_num += remainder;
+        }
+        res
+    }
+}
