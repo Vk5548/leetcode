e748d00f1a5de651f705451d38889dc2b03912ac
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-18 08:32:08 -0500

    Time: 57 ms (0%), Space: 10 MB (0%) - LeetHub

---
+++ 2257-earliest-possible-day-of-full-bloom/2257-earliest-possible-day-of-full-bloom.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn earliest_full_bloom(plant_time: Vec<i32>, grow_time: Vec<i32>) -> i32 {
+        //get the combined array
+        let mut combined : Vec<Vec<i32>> = grow_time.into_iter().zip(plant_time.into_iter()).map(|(g, p)| vec![g, p]).collect();
+
+        //get the sorted array
+        combined.sort_by(|x, y| y[0].cmp(&x[0]).then(y[1].cmp(&x[1]))); //sort by growTime and then break ties using plantTime in desc
+
+        let mut num_days = 0;
+        //start planting on day 0, for first combined[0][1] days nothing else can be done
+
+        // num_days += combined[0][1]; //first seed planted
+
+        let n = combined.len();
+        let mut current_plant = 0;
+        let mut max_bloom_day = 0;
+
+        while current_plant < n{
+            //plant the current plant
+            num_days += combined[current_plant][1]; //planted
+            let curr_bloom_day = num_days + combined[current_plant][0]; //bloom time
+            max_bloom_day = max_bloom_day.max(curr_bloom_day);
+            current_plant += 1;
+        }
+        max_bloom_day
+    }
+}
