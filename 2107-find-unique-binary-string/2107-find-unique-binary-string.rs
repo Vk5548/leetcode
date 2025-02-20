4ec7ba7ae843ea04814eb919c994d19fa753b76b
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-20 15:22:54 -0500

    Time: 0 ms (100%), Space: 2.3 MB (12.5%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.rs
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.rs
@@ -1,33 +1,16 @@
 use std::collections::HashSet;
 impl Solution {
     pub fn find_different_binary_string(nums: Vec<String>) -> String {
-        //convert it into a set
+        // get the set and length
         let n = nums.len();
-        let set = nums.into_iter().collect();
-        let mut candidate = vec!['0'; n];
-        let mut res = None;
-        Self::backtrack(n, 0, &mut candidate, &set, &mut res);
-        res.unwrap()
+        let set: HashSet<String> = nums.into_iter().collect();
 
-    }
-    fn backtrack(n: usize, idx: usize, candidate : &mut Vec<char>, set : &HashSet<String>, res : &mut Option<String>){
-        //base case: when we have completed the candidate upto index n
-
-        if idx == n{
-            //form the Vec<Char> into String
-            let s: String = candidate.iter().collect();
-            if !set.contains(&s){
-                *res = Some(s);
-            }
-            return;
-        }
-
-        for &bit in &['0', '1']{
-            candidate[idx] = bit;
-            Self::backtrack(n, idx + 1, candidate, set, res);
-            if res.is_some(){
-                return
+        for i in 0..=n{
+            let candidate = format!("{:0width$b}", i, width = n);
+            if !set.contains(&candidate){
+                return candidate;
             }
         }
+        unreachable!("there has to be a string which is not in the set as we are iterating through n + 1");
     }
 }
