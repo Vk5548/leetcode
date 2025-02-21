a1247c2685a3c9ad0954821e66c555b2bab366fe
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-02-21 14:38:46 -0500

    Time: 42 ms (61.15%), Space: 45.2 MB (79.33%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -1,59 +1,47 @@
-
-
-import static java.util.Collections.min;
-
-
-
-
+import java.util.*;
 
 class Solution {
-    int m, n;
-    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     public int minimumEffortPath(int[][] heights) {
-        //using Dijkstra's algo
-        m = heights.length;
-        n = heights[0].length;
-
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((n1, n2) -> n1[2] - n2[2]); // row, col, effort
-        //since we have to find the path with minimum effort
-        //we get minimum effort
-        //initialize the array
+        int m = heights.length;
+        int n = heights[0].length;
         int[][] efforts = new int[m][n];
         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                efforts[i][j] = Integer.MAX_VALUE;
-            }
-        }
+            Arrays.fill(efforts[i], Integer.MAX_VALUE);
+        }// go the array
 
-         //we start from (0, 0) : so effort required to reach itself is 0
-        efforts[0][0] = 0;
+        //stsrt form source
+        efforts[0][0] = 0; 
+        //effort, row, col
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((n1, n2) -> n1[0] - n2[0]); //minHeap
+        minHeap.add(new int[]{0, 0, 0});
 
-        minHeap.add(new int[]{0, 0, 0}); // row, col, effort
         while(!minHeap.isEmpty()){
-            int[] cell = minHeap.poll();
-            int r = cell[0], c = cell[1], effort = cell[2];
+            int[] arr = minHeap.poll();
+            int effort = arr[0];
+            int row = arr[1];
+            int col = arr[2];
 
-            if(r == m-1 && c == n-1){ // we reached the detination
+            if(row == m-1 && col == n-1){
                 return effort;
             }
+            if (effort > efforts[row][col]) continue;
 
-            //to get the neighbors from current cell
+            //traversing in all directions
             for(int[] dir: directions){
-                int row = r + dir[0], col = c +dir[1];
-                if(isValid(row, col)){
-                    int newEffort = Math.max(effort, Math.abs(heights[r][c] - heights[row][col])); //for a path we only consider maximum effort
-                    if(newEffort < efforts[row][col]){//push it onto the heap
-                        minHeap.add(new int[]{row, col, newEffort});
-                        efforts[row][col] = newEffort;
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+                if(newRow >= 0 && newRow < m && newCol >= 0 && newCol < n){
+                    int newMaxEffort = Math.max(effort, Math.abs(heights[newRow][newCol] - heights[row][col]));
+                    if (newMaxEffort < efforts[newRow][newCol]){
+                        efforts[newRow][newCol] = newMaxEffort;
+                        minHeap.offer(new int[]{newMaxEffort, newRow, newCol});
                     }
                 }
+               
                 
             }
         }
-
-       return 0;
-    }
-    private boolean isValid(int x, int y){
-        return x >= 0 && x < m && y >= 0 && y < n;
+        return 0;
     }
 }
