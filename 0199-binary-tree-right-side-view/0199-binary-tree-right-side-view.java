17e6f51ce6d2432294bc85a4f92cad77371f162d
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-20 11:14:03 -0400

    Time: 1 ms (71.12%), Space: 42.2 MB (43.08%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,34 +14,44 @@
  * }
  */
 
+ /*
+ root
+ Q ; if root is null
+ Q: of just 1 root
+ Q: the number of nodes
 
+
+ // do a BFS
+ // use a Queue 
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         if(root == null){
             return new ArrayList<>();
         }
-        Queue<TreeNode> que = new LinkedList<>();
-        que.add(root);
-        List<Integer> result = new ArrayList<>();
-        while(!que.isEmpty()){
-            int size = que.size();
-            int mostRightVal = 0;
-            for(int i = 0; i < size; i++){
-                TreeNode node = que.poll();
-                
-                mostRightVal = node.val;
-                //not storing any left child
+        
+        List <Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            TreeNode last = null;
+            for (int i = 0; i < size; i++){
+                TreeNode node = q.poll();
                 if(node.left != null){
-                    que.add(node.left);
+                    q.offer(node.left);
                 }
                 if(node.right != null){
-                    que.add(node.right);
+                    q.offer(node.right);
                 }
-                //add to the result list
-                
+                last = node;
             }
-            result.add(mostRightVal);
+            //done traversing teh current level
+            res.add(last.val);
         }
-        return result;
+
+        return res;
     }
 }
