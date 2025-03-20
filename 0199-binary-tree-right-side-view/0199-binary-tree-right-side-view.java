434da8873853d6742bb62139c3a1e7799f7e3c09
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-20 11:29:19 -0400

    Time: 1 ms (71.12%), Space: 42.6 MB (8.6%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -20,25 +20,27 @@
  Q: of just 1 root
  Q: the number of nodes
 
+ T: 
+
 
  // do a BFS
  // use a Queue 
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if(root == null){
+        if(root == null){ // O(1)
             return new ArrayList<>();
         }
         
-        List <Integer> res = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        List <Integer> res = new ArrayList<>(); //O(1)
+        Queue<TreeNode> q = new LinkedList<>(); //O(199)
+        q.add(root); // O(1)
 
-        while(!q.isEmpty()){
+        while(!q.isEmpty()){ //O(n)
             int size = q.size();
 
             TreeNode last = null;
-            for (int i = 0; i < size; i++){
+            for (int i = 0; i < size; i++){ //O(size)
                 TreeNode node = q.poll();
                 if(node.left != null){
                     q.offer(node.left);
