f589431caf04a1d0d62172d20f3b4e1a66992843
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-20 11:38:36 -0400

    Time: 61 ms (52.2%), Space: 61.7 MB (19.36%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,17 +1,41 @@
+
+
+import static java.lang.Math.min;
+
+
+/* 
+nums; k, kth largest element
+kth largest elemnt in sorted order not the
+ nums[size] : 10^5
+heap solution: 
+min-heap and maintain its size , while adding element
+//at the end of the traversal, return the last element
+// T: O(nlogk )+ O(logk)
+S: O(k)
+
+A2:
+take the resultant array od mazimum size ofnums
+-iterate through the array
+-and keep increasing the freq of the array and//after the input array traversal, we can just raverse the array of freq from higher degree,
+//T: O(n) + O(size of the res array)
+//S: O(max-size of [nums[i]])
+
+//HashhMAp, : num: freq
+solv eit suing sorted hashmap 
+*/
+
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> n2 - n1);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((n1, n2) -> n1 - n2);
 
-        //add all the elelmenst in the maxHeap
-        for(int n: nums){
-            maxHeap.add(n);
-        }
-        int i = 1;
-        while(i < k){
-            maxHeap.poll();
-            i++;
-        }
-        return maxHeap.poll();
+        for(int num : nums){
+            minHeap.offer(num);
 
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
+        }
+        return minHeap.poll();
     }
 }
