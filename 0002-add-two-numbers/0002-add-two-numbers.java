3fe23d61509d1baa0826c83514a9006a4ce56aed
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-21 17:30:23 -0400

    Time: 1 ms (100%), Space: 44.8 MB (21%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,56 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /* 
+ a LL, non-empty, non-negative, >=0, 
+ sorted in reverse order, add 2 nums and return the sum as LL
+
+ Q:What if one of the ll is empty?
+ Q: what is the max length of 1 link list?
+ Q: // 234 => 4 -> 3> 2? 
+ Q: zre there leading zeroes
+ 
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-          /*
-    Name - Add Two Numbers
-    Link - https://leetcode.com/problems/add-two-numbers/
-    Time Complexity - O(m + n)
-    Space Complexity - o(1)
-    Note - make use of modulo (get remainder) and division (get quotient)
-*/
+        if (l1 == null && l2 == null){
+            return null;
+        }
+        if(l1 == null || l2 == null){
+            return l1 == null ? l2 : l1;
+        }
 
+        ListNode res = new ListNode(0);
+        ListNode holder = res;
 
-        int carry = 0;
-        int remainder = 0;
-        int sum = 0;
-        ListNode head = null;
-        ListNode temp = null;
-        while (l1 != null || l2 != null) {
-            sum =
-                carry +
-                (
-                    ((l1 != null) ? l1.val : 0) +
-                    ((l2 != null) ? l2.val : 0)
-                );
-            remainder = sum % 10;
+        int sum = 0, carry = 0;
+
+        //iterate 
+        while(l1 != null || l2 != null ){
+            sum = carry + (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0);
+
+            int remainder = sum % 10; //new ListNode
             carry = sum / 10;
             ListNode newNode = new ListNode(remainder);
-            if (head == null) {
-                head = newNode;
-            } else {
-                temp = head;
-                while (temp.next != null) {
-                    temp = temp.next;
-                }
-                temp.next = newNode;
-                newNode.next = null;
-            }
-            l1 = l1 != null ? l1.next: null;
-            l2 = l2 != null ? l2.next: null;
-        }
-        if (carry > 0) {
-            ListNode newNode = new ListNode(carry);
-            temp = head;
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-            newNode.next = null;
-        }
-        return head;
-    }
-}
+            res.next = newNode;
+            res = newNode;
+            res.next = null;
 
- 
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+        }
+        if(carry > 0){
+            res.next = new ListNode(carry);
+            res = res.next;
+            res.next = null;
+        }
+        return holder.next;
+
+    }
+   
+}
