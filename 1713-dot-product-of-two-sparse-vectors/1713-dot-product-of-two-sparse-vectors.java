c24b9b8488a7688c13e0ecbea4a9a962a18cd861
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-22 09:37:21 -0400

    Time: 13 ms (14.16%), Space: 59.3 MB (8.34%) - LeetHub

---
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.java
@@ -0,0 +1,32 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+class SparseVector {
+    // how about we store than in hashmap: {idx: num}
+    SortedMap<Integer, Integer> nonZero = new TreeMap<>();
+    SparseVector(int[] nums) {
+       for(int i = 0; i < nums.length; i++){
+          if(nums[i] != 0){
+            nonZero.put(i, nums[i]);
+        }
+       }
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        //we iterate through the map and get the 
+        int result= 0;
+
+        for(Integer key : nonZero.keySet()){
+            if(vec.nonZero.containsKey(key)){
+                result += vec.nonZero.get(key) * this.nonZero.get(key);
+            }
+        }
+        return result;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
