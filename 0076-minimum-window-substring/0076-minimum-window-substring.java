5201043ce762fed683a4e4950f454ae215045fbb
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-28 12:07:30 -0400

    Time: 20 ms (45.35%), Space: 45.4 MB (42.71%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,65 @@
+/* 
+s: m 
+t : n
+return the min window substring of s, such that
+every charactre of t is included in the window(incl. duplicates)
+
+T1: "ADOBECODEBANC" , "ABC" 
+// every character of t should be in teh substring
+and order doesn't matter
+
+A1: sliding window
+// a list for all characters of t
+
+*/
+
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) return "";
+            
+        Map<Character, Integer> tFreq = new HashMap<>();
+        for(char c: t.toCharArray()){
+            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int formed = 0;
+        int required = tFreq.size();
+
+        Map<Character, Integer> windowFreq = new HashMap<>();
+        int[] ans = {-1, 0, 0}; //length, left, right
+
+        while(right < s.length()){
+            char c = s.charAt(right);
+            windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
+
+            if(tFreq.containsKey(c) && windowFreq.get(c).intValue() == tFreq.get(c).intValue()){
+                formed++;
+            }
+
+            while(left <= right && formed == required){
+                c = s.charAt(left);
+
+                if(ans[0] == -1 || right - left + 1 < ans[0]){
+                    ans[0] = right - left + 1;
+                    ans[1] = left;
+                    ans[2] = right;
+                }
+
+                //Remove form window
+                windowFreq.put(c, windowFreq.get(c) - 1);
+                if (tFreq.containsKey(c) && windowFreq.get(c).intValue() < tFreq.get(c).intValue()) {
+                    formed--;
+                }
+
+                left++;
+
+            }
+            right++;
+        }
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        
+    }
+    
+}
