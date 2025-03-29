582084a948e5e0676d4e1775eb4a93460aa19c3e
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-29 13:01:01 -0400

    Time: 33 ms (39.39%), Space: 51.2 MB (65.7%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,24 +1,37 @@
+/* 
+points[[]]= points[i] = [xi, yi]
+k closest to origin
+
+CQ: what if points.length < k?, return as is?
+what id points.length = 0? , k >0, : retrun an empty arr
+what if n > 0, k = 0, retrun an empty arr
+
+*/
+
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> heap = new PriorityQueue<>((n1, n2) -> {
-           // Calculate distances and compare
+        int n = points.length;
+        if (n < k) return points;
+        if( n == 0 || k == 0) return new int[0][0];
+    
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((n1, n2) -> {
             int d1 = n1[0] * n1[0] + n1[1] * n1[1];
             int d2 = n2[0] * n2[0] + n2[1] * n2[1];
-            return d2 - d1; 
-        }); // max-heap
+            return d2 - d1;
+        });
 
-        for(int[] point: points){
-            heap.add(point);
-            if(heap.size() > k){
-                heap.poll();
+        for(int[] p : points){
+            maxHeap.offer(p);
+            if(maxHeap.size() > k){
+                maxHeap.poll();
             }
         }
-
         int[][] res = new int[k][2];
         int i = 0;
-        while(heap.size() > 0){
-            res[i++] = heap.poll();
-        }
+        while(!maxHeap.isEmpty())
+            res[i++] = maxHeap.poll();
+
+
         return res;
     }
 }
