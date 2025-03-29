275377be4df7c86e402e3fce4f6f78e5b6c1e7f5
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-29 15:58:44 -0400

    Time: 1 ms (80.58%), Space: 41.8 MB (35.56%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -1,39 +1,28 @@
-/**
-given : order , s 
-all charac in order : Unique , what bout the character in the s
+/* 
+order and s 
+// achars in order are custom and uniique
+// get the chracters from s 
+and store it in an count array, concists of lower case characters
 
-amnd sorted in some order 
 
-permute the order of s such taht 
-
-turn into a Set
-oterate through sarr and append the characetrs to sb in the order they are in the Set
- */
+*/
 
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character, Integer> freq = new HashMap<>();
-        for(char c: s.toCharArray())
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-
-
+        int[] count = new int[26];
+        for(char c : s.toCharArray())
+            count[c - 'a']++;
+        
         StringBuilder sb = new StringBuilder();
-        for (char c : order.toCharArray()){
-            if(freq.containsKey(c)){
-                int count = freq.get(c);
-                for(int i = 0; i < count; i++)
-                    sb.append(c);
-                freq.remove(c);
-            }
+        for(char c: order.toCharArray()){
+            sb.append(String.valueOf(c).repeat(count[c - 'a']));
+            // for(int i = 0; i < count[c - 'a']; i++)
+            //     sb.append(c);
+            count[c - 'a'] = 0;
         }
-
-        //remaining characetrs
-        for(char c: freq.keySet()){
-            int count = freq.get(c);
-            for(int i = 0; i < count; i++)
-                sb.append(c);
+        for(int i = 0; i < 26; i++){
+            sb.append(String.valueOf((char)('a' + i)).repeat(count[i]));
         }
         return sb.toString();
-
     }
 }
