786b7a146db971708bcc38733c1a221e973ae089
Vaidehi Kalra <97982128+Vk5548@users.noreply.github.com>
2025-03-29 11:13:50 -0400

    Time: 19 ms (52.62%), Space: 45.4 MB (35.6%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,66 +1,77 @@
 /* 
-s: m 
-t : n
-return the min window substring of s, such that
-every charactre of t is included in the window(incl. duplicates)
+Given : s and t :of length m and t 
+minimum window substring
+such taht every character in is in the wondow, return the window
 
-T1: "ADOBECODEBANC" , "ABC" 
-// every character of t should be in teh substring
-and order doesn't matter
+CQ: 
+1 Is the ans unique?
+2 when s = "" and t = "", return ""? Y
+3 when s = "<some string>", t = "", return ""
+4 s = "" and t= "<some string>"", return ""
 
-A1: sliding window
-// a list for all characters of t
+A: keep tarck of t with a. hashmap, storing freq of teh characetrs
+-> form a window, again also using a hashmap to keep of current characetrs
+and and if it matches, keep the track if the res in minimum
+try to decrease the window
+ T: O(m+n)
+ S: O(n)
+
+A -> use a a count array of 52? for both a and A
+T: O(n)
+S: O(n)
+
 
 */
-
-
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() == 0 || t.length() == 0) return "";
-            
-        Map<Character, Integer> tFreq = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+        
         for(char c: t.toCharArray()){
-            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
+        int required = tMap.size();
 
         int left = 0, right = 0;
-        int formed = 0;
-        int required = tFreq.size();
+        int len = s.length();
 
-        Map<Character, Integer> windowFreq = new HashMap<>();
-        int[] ans = {-1, 0, 0}; //length, left, right
+        Map<Character, Integer> currMap = new HashMap<>();
+        int current = 0;
+        int[] ans = {-1, 0, 0}; // len, left, right
 
-        while(right < s.length()){
+        while ( right < len ){
             char c = s.charAt(right);
-            windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
+            currMap.put(c, currMap.getOrDefault(c, 0) + 1);
 
-            if(tFreq.containsKey(c) && windowFreq.get(c).intValue() == tFreq.get(c).intValue()){
-                formed++;
-            } // got a avlid substring when formed == required
+            if(tMap.containsKey(c) && tMap.get(c).intValue() == currMap.get(c).intValue()){ // where we have gotten a correct
+                current++;
+            }
 
-            while(left <= right && formed == required){
+
+            while (left <= right && required == current){
                 c = s.charAt(left);
+                
+                
 
-                if(ans[0] == -1 || right - left + 1 < ans[0]){ //checking if we get the a lesser value
+                if(ans[0] == -1 || right - left + 1 < ans[0]){
                     ans[0] = right - left + 1;
                     ans[1] = left;
                     ans[2] = right;
-                }
+                }//updated the ans
 
-                //Remove form window
-                windowFreq.put(c, windowFreq.get(c) - 1);
-                // if(windowFreq.get(c) == 0) windowFreq.remove(c);
-                if (tFreq.containsKey(c) && windowFreq.get(c).intValue() < tFreq.get(c).intValue()) {
-                    formed--;
+                currMap.put(c, currMap.get(c) - 1);
+                if(tMap.containsKey(c) && tMap.get(c).intValue() > currMap.get(c).intValue()){
+                    current--;
                 }
 
                 left++;
 
             }
+
             right++;
         }
+
         return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
-        
+
+
     }
-    
 }
